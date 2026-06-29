241a434f3a2078d7c3b77af4295bdf0246521901
Anika Pawa <anikapawa@Anikas-MacBook-Pro.local>
2026-06-29 13:55:59 -0400

    Updated grid book club

--- app/book-club/page.tsx
+++ app/book-club/page.tsx
@@ -183,7 +183,7 @@ export default function BookClubPage() {
         </h2>
 
         {/* FULL WIDTH GRID USING BOOKCARD */}
-        <div className="flex flex-wrap gap-6">
+        <div className="flex flex-wrap gap-8">
             {pastSelections.map((book) => (
                 <BookCard key={book.id} book={book} />
             ))}
